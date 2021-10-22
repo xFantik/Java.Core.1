18b61203977326b383367ec845ccca8eb4f7962d
Fantik <phantom88@inbox.ru>
2021-10-22 14:42:48 +0300

    Add HomeWork_1

--- src/main/java/ru/gb/home_works/lesson_1/HomeWorkApp.java
+++ src/main/java/ru/gb/home_works/lesson_1/HomeWorkApp.java
@@ -6,6 +6,60 @@ public class HomeWorkApp {
         checkSumSign();
         printColor();
         compareNumbers();
+        quadratic(-2,6,9); //Доп задание из телеги (Квадратное уравнение)
+
+
+
+
+
+    }
+
+
+
+    public static void quadratic(int a, int b, int c) {
+        System.out.println("\n\n\n");
+        if (a==0 && b!=0){
+            System.out.println("Это линейное уравнение: "+b+"x+"+c+"=0");
+            double  x = (-(double)c) /b ;
+            System.out.println("Ответ: x="+x);
+        }
+        else if (a==0 && b==0){             //Вот тут пришлось возврат метода делать void (или exception, которые еще надо изучать)
+            if (c==0){
+                System.out.println("Упрощаем уравнение: 0=0. Эй, куда делись иксы?");
+            }
+            else{
+                System.out.println("Если а = 0 и b = 0, Вы утверждаете, что "+ c + " = 0. Мне нравится Ваш подход =))");
+            }
+        }
+
+        else {
+            System.out.println("Пытаемся решить уравнение: " + a + "x²+" + b + "x+" + c + "=0");
+            System.out.println("Считаем дискрименант по формуле D = b² - 4ac");
+            System.out.println("Подставляем значения: D=" + b + "²-4*" + a + "*" + c);
+            double d = 1.0 * b * b - (4.0 * a * c);
+            System.out.println("Дискрименант D = " + d);
+            if (d<0){
+                System.out.println("D < 0, значит уравнение не имеет решения");
+            }
+            else if (d==0){
+                System.out.println("Ищем корень уравнения по формуле: x1 = -b / 2a");
+                System.out.println("Уравнение имеет один корень:\nx = "+ (-b / 2.0 * a));
+            }
+            else{
+                System.out.println("Ищем корни уравнения по формуле: x1 = (-b + √D) / 2a");
+                System.out.println("Ищем корни уравнения по формуле: x2 = (-b + √D) / 2a");
+
+                double x1 = (-b + Math.sqrt(d)) / 2.0 * a;
+                double x2 = (-b - Math.sqrt(d)) / 2.0 * a;
+
+                System.out.println("Ответ:\nx1 = "+x1+"\nx2 = " + x2);
+            }
+
+        }
+
+
+
+
     }
 
     public static void compareNumbers(){

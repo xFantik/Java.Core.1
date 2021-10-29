18630f14f936f289f091389434b4448e7a96b750
xFantik <phantom88@inbox.ru>
2021-10-29 14:38:05 +0300

    Выполнено Домашнее Задание 3

--- src/main/java/ru/gb/home_works/lesson_3/HomeWorkLesson_3.java
+++ src/main/java/ru/gb/home_works/lesson_3/HomeWorkLesson_3.java
@@ -3,32 +3,68 @@ package ru.gb.home_works.lesson_3;
 public class HomeWorkLesson_3 {
     public static void main(String[] args) {
 //1         Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
-
-
+        int[] array = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) array[i] = 1;
+            else array[i] = 0;
+        }
+        System.out.print("Задание 1: ");
+        arrayPrinter(array);
 
 //2         Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
-
-
+        array=new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i]=i+1;
+        }
+        System.out.print("\nЗадание 2: ");
+        arrayPrinter(array);
 
 
 //3        Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+        array = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]<6)
+                array[i]*=2;
+        }
+        System.out.print("\nЗадание 3: ");
+        arrayPrinter(array);
 
 
-
-//4        Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+//4        Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+//          и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
+//          Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+        int size=10;
+        int[][] array2d=new int[size][size];
+        for (int i = 0; i < array2d.length; i++) {
+            for (int j = 0; j < array2d[i].length; j++) {
+                if (i==j || j+i==array2d.length-1) array2d[i][j]=1;
+            }
+        }
+        System.out.print("\nЗадание 4: ");
+        arrayPrinter(array2d);
 
 
 //5        Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
+        System.out.print("\nЗадание 5: ");
+        arrayPrinter(createArray(10, -5));
 
+        
 //6        Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+        System.out.print("\nЗадание 6: ");
+        array = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]>max) max=array[i];
+            if (array[i]<min) min=array[i];
+        }
+        System.out.printf("\nМинимальное значение массива: %d\nМаксимальное значение массива: %d\n", min, max);
 
 
 //7        Написать метод, в который передается не пустой одномерный целочисленный массив,
 //        метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
-        int[] array=new int[]{1,2,3,4,2,0};
-
-
-        System.out.println(leftAndRightSum(new int[]{-10,10,0,0,0,0,0}));
+        System.out.print("\nЗадание 7: ");
+        System.out.println(leftAndRightSum(new int[]{ 10,-10, 2, 18,20}));
 
 
 //8        Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным),
@@ -36,41 +72,54 @@ public class HomeWorkLesson_3 {
 //          Для усложнения задачи нельзя пользоваться вспомогательными массивами.
 //          Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ].
 //          При каком n в какую сторону сдвиг можете выбирать сами.
+        System.out.print("\nЗадание 8: ");
+        shiftArrayWithoutSecondArray(new int[]{1, 2, 3,4,5 }, -201);
 
 
     }
+    //задание 5
+    private static int[] createArray(int len, int initialValue) {
+        if (len<=0) return null;
+        int[] arr=new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= initialValue;
+        }
+        return arr;
+    }
 
+    //задание 7
     private static boolean leftAndRightSum(int[] array) {
-        int startSumm = 0;
-        int endSumm = 0;
+        int leftSum = 0;
+        int rightSum = 0;
         int i = 0, j = array.length - 1;
         while (i <= j) {
-            if (startSumm <= endSumm) {
-                startSumm += array[i];
+            if (leftSum <= rightSum) {
+                leftSum += array[i];
                 i++;
             } else {
-                endSumm += array[j];
+                rightSum += array[j];
                 j--;
             }
         }
-        if (startSumm == endSumm) {
+        if (leftSum == rightSum) {
             leftAndRightSumPrinter(array, i);
             return true;
         } else return false;
     }
 
+    //задание 7 вывод
     public static void leftAndRightSumPrinter(int[] array, int endIndex) {
-        if(array.length==endIndex){
+        if (array.length == endIndex) {
             System.out.println("Сумма всех элементов массива равна 0");
             return;
         }
         StringBuilder result = new StringBuilder();
         result.append("{");
         for (int i = 0; i < array.length; i++) {
-            if (i < endIndex-1) {
+            if (i < endIndex - 1) {
                 result.append(array[i]);
                 result.append(" + ");
-            } else if (i == endIndex-1) {
+            } else if (i == endIndex - 1) {
                 result.append(array[i]);
                 result.append(" = ");
             } else if (i < array.length - 1) {
@@ -83,4 +132,56 @@ public class HomeWorkLesson_3 {
         }
         System.out.println(result);
     }
+    //задание 8
+    private static void shiftArray(int[] array, int shift) {
+        int arrayNew[]=new int[array.length];
+        while (shift <0) shift += array.length;
+        for (int i = 0; i < array.length; i++) {
+            arrayNew[Math.abs(i+ shift)% array.length]= array[i];
+        }
+        arrayPrinter(arrayNew);
+    }
+
+    //задание 8*
+    private static void shiftArrayWithoutSecondArray(int[] array, int shift) {
+        while (shift <0) shift += array.length;
+        shift %= array.length;
+        for (int i = 0; i < shift; i++) {
+            int tmp= array[array.length-1];
+            for (int j = array.length-1; j >0; j--) {
+                array[j]= array[j-1];
+            }
+            array[0]=tmp;
+        }
+        arrayPrinter(array);
+    }
+
+    public static void arrayPrinter(int[] array) {
+        if (array==null) {
+            System.out.println("Массив не проинициализирован");
+            return;
+        }
+        StringBuilder result = new StringBuilder();
+        result.append("{");
+        for (int i = 0; i < array.length; i++) {
+            result.append(array[i]);
+            if (i < array.length - 1)
+                result.append(", ");
+            else
+                result.append("}");
+        }
+        System.out.println(result);
+    }
+    public static void arrayPrinter(int[][] array) {
+        StringBuilder result = new StringBuilder();
+        result.append("\n");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                result.append(array[i][j]+ " ");
+            }
+            result.append("\n");
+        }
+        System.out.println(result);
+    }
+
 }
